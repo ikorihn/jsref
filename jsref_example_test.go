de0a62ab0958d2615524c7974cce3da3eaa512b4
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-07-18 23:17:14 +0900

    Update example reading yaml file

--- jsref_example_test.go
+++ jsref_example_test.go
@@ -3,33 +3,45 @@ package jsref_test
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 
-	jsref "github.com/lestrrat-go/jsref"
-	"github.com/lestrrat-go/jsref/provider"
+	"github.com/goccy/go-yaml"
+	jsref "github.com/r57ty7/jsref"
+	"github.com/r57ty7/jsref/provider"
 )
 
 func Example() {
 	var v interface{}
-	src := []byte(`
-{
-  "foo": ["bar", {"$ref": "#/sub"}, {"$ref": "obj2#/sub"}],
-  "sub": "baz"
-}`)
-	if err := json.Unmarshal(src, &v); err != nil {
+	f, err := os.Open("testfile/foo.yaml")
+	if err != nil {
+		log.Printf("ファイルエラー: %v", err)
+		return
+	}
+	src, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Printf("ファイルreadエラー: %v", err)
+		return
+	}
+	if err := yaml.Unmarshal(src, &v); err != nil {
 		log.Printf("%s", err)
 		return
 	}
 
 	// External reference
-	mp := provider.NewMap()
-	mp.Set("obj2", map[string]string{"sub": "quux"})
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Printf("url parse: %s", err)
+		return
+	}
+	mp := provider.NewFS(wd + "/testfile/")
 
 	res := jsref.New()
 	res.AddProvider(mp) // Register the provider
 
 	data := []struct {
-		Ptr string
+		Ptr     string
 		Options []jsref.Option
 	}{
 		{
@@ -42,12 +54,12 @@ func Example() {
 			Ptr: "#/foo/2", // "quux" (resolves via `mp`)
 		},
 		{
-			Ptr: "#/foo",   // ["bar",{"$ref":"#/sub"},{"$ref":"obj2#/sub"}]
+			Ptr: "#/foo", // ["bar",{"$ref":"#/sub"},{"$ref":"obj2#/sub"}]
 		},
 		{
-			Ptr: "#/foo",   // ["bar","baz","quux"]
+			Ptr: "#/foo", // ["bar","baz","quux"]
 			// experimental option to resolve all resulting values
-			Options: []jsref.Option{ jsref.WithRecursiveResolution(true) },
+			Options: []jsref.Option{jsref.WithRecursiveResolution(true)},
 		},
 	}
 	for _, set := range data {
@@ -63,7 +75,7 @@ func Example() {
 	// OUTPUT:
 	// #/foo/0 -> "bar"
 	// #/foo/1 -> "baz"
-	// #/foo/2 -> "quux"
-	// #/foo -> ["bar",{"$ref":"#/sub"},{"$ref":"obj2#/sub"}]
-	// #/foo -> ["bar","baz","quux"]
+	// #/foo/2 -> "piyo"
+	// #/foo -> ["bar",{"$ref":"#/sub"},{"$ref":"file://./sub.yaml#/other"}]
+	// #/foo -> ["bar","baz","piyo"]
 }
